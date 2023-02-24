a1c01e9fd30e1a1b87f8bce6c7272f176e1c7aed
Andrii Gargat <andreysax1312@gmail.com>
2023-02-24 19:25:56 +0200

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,60 @@
-import { Counter } from './Counter/Counter';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <h2>РАХІВНИЦЯ</h2>
-      <Counter initialValue={100}>
-        
-      </Counter>
-    </div>
-  );
-};
+
+export class App extends Component {
+  state = {
+    inputValue: '',
+    name: '',
+    surname: ''
+  };
+
+  handleChange = event => { 
+    const { name, value } = event.currentTarget;
+    this.setState({[name]: value})
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#690808',
+        }}
+      >
+        {/* <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInput}
+        /> */}
+
+        <form style={{         
+          display: 'flex',
+          flexDirection: 'column',
+        }}>
+          <label htmlFor="">
+            Name <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="">
+            Surname <input
+              type="text"
+              name="surname"
+              value={this.state.surname}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+
+      </div>
+    );
+  }
+}
